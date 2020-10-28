fc0332de0f1a87ad788152c4c77f11e1bd437d44
Joseph B. Kell <JBKell@co.anoka.mn.us>
2020-10-28 12:42:32 -0500

    Able to make a csv but there are blank lines.

--- pullCSV.py
+++ pullCSV.py
@@ -3,10 +3,25 @@ from bs4 import BeautifulSoup
 import csv
 from pprint import pprint
 import urllib.request
+from io import StringIO
+
 url="https://spotifycharts.com/regional/sv/daily/latest/download"
 response=requests.get(url)
-SpicyArray=str(response.content).split(chr(92)+"n")
-print(f"The length is {len(SpicyArray)}")
-for car in SpicyArray:
-    car=car.split(",")
-    print(car, len(car))
+# SpicyArray=str(response.content).split(chr(92)+"n")
+# reader= csv.reader(SpicyArray,delimiter=',')
+# for row in reader:
+#     print(row)
+#print(f"The length is {len(SpicyArray)}")
+
+reader= csv.reader(str(response.content).split(chr(92)+"n"),delimiter=",")
+
+outPath="Output/test.csv"
+
+with open(outPath, 'w') as csvfile:
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=',')
+
+    for row in reader:
+
+        csvwriter.writerow(row)
+        print(row)
